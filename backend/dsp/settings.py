2b9c1ff02d139cf7980c7b352c63c4ff6d4a62f2
Abhishek <abhishekaryz@gmail.com>
2024-12-28 15:53:58 +0530

    commit

--- backend/dsp/settings.py
+++ backend/dsp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY", default="unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
