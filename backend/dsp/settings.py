d17ed37e738804ea9442e24d7ac8d2019788e458
Abhishek <abhishekaryz@gmail.com>
2024-12-30 15:58:02 +0530

    git push

--- backend/dsp/settings.py
+++ backend/dsp/settings.py
@@ -132,7 +132,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "https://ad-campaign.vercel.app/",
+    "https://ad-campaign.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
