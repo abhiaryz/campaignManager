509572215c458c8723807f0a4e33e346fb1dc950
Abhishek <abhishekaryz@gmail.com>
2024-12-31 16:00:19 +0530

    REFRESH_TOKEN_LIFETIME

--- backend/dsp/settings.py
+++ backend/dsp/settings.py
@@ -80,7 +80,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=15),
 }
 
 
