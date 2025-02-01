0a88e0df2d7d9b587a646b192483ebf8fe4bd145
Abhishek <abhishekaryz@gmail.com>
2025-02-01 21:04:17 +0530

    update

--- backend/dsp/settings.py
+++ backend/dsp/settings.py
@@ -177,7 +177,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Media files
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 MEDIA_ROOT = "media/"
 INSTALLED_APPS += ["rest_framework_simplejwt.token_blacklist"]
 
