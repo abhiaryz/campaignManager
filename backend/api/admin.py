196784cd3e2fd9a1ef00351444996481f75924c0
Abhishek <abhishekaryz@gmail.com>
2025-02-01 18:53:26 +0530

    admin modfied

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -40,6 +40,38 @@ class UserTypeAdmin(admin.ModelAdmin):
     ordering = ("user",)
 
 
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ("language", "iso_code", "label")
+    search_fields = ("language",)
+    list_filter = ("language",)
+    ordering = ("language",)
+
+class ExchangeAdmin(admin.ModelAdmin):
+    list_display = ("exchange", "label")
+    search_fields = ("exchange",)
+    list_filter = ("exchange",)
+    ordering = ("exchange",)
+
+class CarrierDataAdmin(admin.ModelAdmin):
+    list_display = ("carrier", "label")
+
+class EnvironmentAdmin(admin.ModelAdmin):
+    list_display = ("env", "label")
+
+
+class BrandSafetyAdmin(admin.ModelAdmin):
+    list_display = ("value", "label")
+
+class BuyTypeAdmin(admin.ModelAdmin):
+    list_display = ("value", "label")
+
+class ViewabilityAdmin(admin.ModelAdmin):
+    list_display = ("value", "label")
+
+class target_typeAdmin(admin.ModelAdmin):
+    list_display = ("category", "subcategory")
+
+
 class LocationAdmin(admin.ModelAdmin):
     list_display = ("country", "state", "city", "tier", "population")
     search_fields = ("country",)
@@ -47,27 +79,34 @@ class LocationAdmin(admin.ModelAdmin):
     ordering = ("country",)
 
 
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ("name", "objective", "start_time", "end_time", "status")
+    search_fields = ("name", "objective", "status")
+    list_filter = ("name", "objective", "status")
+    ordering = ("name", "objective", "status")
+
+
 admin.site.register(UserType, UserTypeAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(Campaign)
+admin.site.register(Campaign,CampaignAdmin)
 admin.site.register(Location, LocationAdmin)
-admin.site.register(target_type)
+admin.site.register(target_type, target_typeAdmin)
 admin.site.register(CampaignImage)
 admin.site.register(Keyword)
 admin.site.register(Age)
-admin.site.register(CarrierData)
-admin.site.register(Environment)
-admin.site.register(Exchange)
-admin.site.register(Language)
+admin.site.register(CarrierData, CarrierDataAdmin)
+admin.site.register(Environment, EnvironmentAdmin)
+admin.site.register(Exchange, ExchangeAdmin)
+admin.site.register(Language, LanguageAdmin)
 admin.site.register(Impression)
 admin.site.register(DevicePrice)
 admin.site.register(Device)
 admin.site.register(DistinctInterest)
 admin.site.register(CampaignVideo)
-admin.site.register(BrandSafety)
-admin.site.register(BuyType)
+admin.site.register(BrandSafety, BrandSafetyAdmin)
+admin.site.register(BuyType, BuyTypeAdmin)
 admin.site.register(Bidding_detail)
-admin.site.register(Viewability)
+admin.site.register(Viewability, ViewabilityAdmin)
 
 @admin.register(CityData)
 class CityDataAdmin(admin.ModelAdmin):
