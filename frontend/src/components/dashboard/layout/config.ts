64a7c5ee26435b440e6415323659e8b5a2b4ca47
Gautam <gautam@nirmata.tech>
2025-02-23 14:21:54 +0530

    added creative section

--- frontend/src/components/dashboard/layout/config.ts
+++ frontend/src/components/dashboard/layout/config.ts
@@ -3,6 +3,7 @@ import { paths } from '@/paths';
 
 export const navItems = [
   { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'plugs-connected' },
+  { key: 'creative', title: 'Creative', href: paths.dashboard.creative, icon: 'file-plus' },
   { key: 'customers', title: 'Customers', href: paths.dashboard.customers, icon: 'user' },
   { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
 ] satisfies NavItemConfig[];
